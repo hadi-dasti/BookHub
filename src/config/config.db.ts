8ba3b2e4ac3d454680ba7014073b0c8c0356cbd8
hadi dasti <hadidasti98@hotmail.com>
2024-02-21 01:13:34 +0330

    setup config for connect to mongodbsrc/config/config.db.ts

--- src/config/config.db.ts
+++ src/config/config.db.ts
@@ -3,16 +3,18 @@ import dotenv from 'dotenv';
 import * as path from "path";
 
 
+// Load environment variables from .env files
+dotenv.config({path:path.join(__dirname, './../../.env')});
+
+// config url
+export const MONGODB_URI: string = process.env.MONGODB_URI ; 
+
 // Connect to the MongoDB database
 export async function connectDB():Promise<void> {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/BookHub')
+        await mongoose.connect(MONGODB_URI);
         console.log('mongodb connect successfully ')
     } catch (err) {
         console.log('MongoDB connection error:', err)
     };
 };
-
-
-
-// export default connectDB;
