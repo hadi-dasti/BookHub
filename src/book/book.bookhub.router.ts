2a88d3be39a75b6f501573ba370f3d10fbe4cbde
hadi dasti <hadidasti98@hotmail.com>
2024-02-23 01:53:18 +0330

    book schema in book.bookHub module is update

--- src/book/book.bookhub.router.ts
+++ src/book/book.bookhub.router.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 
+import { BookController } from "./controller/book.bokkhub.controller";
 
 const router: Router = Router();
 
-
+router.post('/create')
 
 
 export default router;
